306fd9e78738456def612c8367a96257636ed6eb
meheraj786 <meherajhosen786@gmail.com>
2025-10-27 03:38:10 +0600

    route bug fix

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const path = require("path");
 
 (async () => {
   try {
+    const cors = require("cors");
+    app.use(
+      cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+      })
+    );
     app.use(express.static(path.join(__dirname, "public/temp")));
     app.use(express.json());
     const port = process.env.PORT;
--- routes/api/category.js
+++ routes/api/category.js
@@ -1,12 +1,12 @@
 const express=require("express")
-const { createCategory, getAllCategory, getSingleCategory, updateSingleCategory } = require("../../controllers/category.controller")
+const { createCategory, getAllCategory, getSingleCategory, updateSingleCategory, deleteSingleCategory } = require("../../controllers/category.controller")
 const router=express.Router()
 
 router.post("/create-category", createCategory)
 router.get("/all-category", getAllCategory)
 router.get("/single-category/:id", getSingleCategory)
 router.patch("/update-category/:id", updateSingleCategory)
-router.delete("/delete-category/:id", createCategory)
+router.delete("/delete-category/:id", deleteSingleCategory)
 
 
 module.exports=router
