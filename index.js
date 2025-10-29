779719ecd244fdd8f939cd3305a2050606b327f0
meheraj786 <meherajhosen786@gmail.com>
2025-10-29 12:13:11 +0600

    cors policy update

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const path = require("path");
 (async () => {
   try {
     const cors = require("cors");
-app.use(cors({ origin: ["http://localhost:5173", "https://techmart-ecom-dashboard.vercel.app"], credentials: true }));
+    app.use(
+      cors({
+        origin: [
+          "http://localhost:5173",
+          "http://localhost:5174",
+          "https://techmart-ecom-dashboard.vercel.app",
+        ],
+        credentials: true,
+      })
+    );
 
     app.use(express.static(path.join(__dirname, "public/temp")));
     app.use(express.json());
